7773202f4e6c1df3f2c6d550fb1aa1334146d523
[dyad] <arone.consult@gmail.com>
2026-01-30 07:18:53 -0300

    [dyad] Ajustando layout mobile para 4 cards na mesma linha - wrote 1 file(s)

--- src/components/sections/PricingSection.tsx
+++ src/components/sections/PricingSection.tsx
@@ -104,7 +104,7 @@ const PricingSection = () => {
           </div>
 
           {/* Pricing Cards */}
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-6 px-4 sm:px-0">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 px-4 sm:px-0">
             {plans.map((plan, index) => (
               <div
                 key={index}
